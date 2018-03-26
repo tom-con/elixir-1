14cda08c1c7750cfdb069b2bf0e66bb5d8d231b9
Thomas Conchie <tconchie@gmail.com>
2018-03-26 16:00:29 -0600

    Completes atbash cipher

--- exercises/atbash-cipher/atbash_cipher.exs
+++ exercises/atbash-cipher/atbash_cipher.exs
@@ -9,9 +9,32 @@ defmodule Atbash do
   """
   @spec encode(String.t()) :: String.t()
   def encode(plaintext) do
+    String.replace(plaintext, ~r/[^\w]/, "")
+      |> String.downcase
+      |> String.graphemes
+      |> Enum.map(&reverse_codes/1)
+      |> Enum.reduce("", fn (l, acc) -> acc <> l end)
+      |> String.replace(~r/(\w{4})(\w)/, "\\g{1}\\g{2} ")
+      |> String.trim
+  end
+
+  def reverse_codes(letter) do
+    cond do
+      String.match?(letter, ~r/[a-z]/) ->
+        <<x, 0>> = letter <> <<0>>
+        c = (122 - (x-97))
+        <<c::utf8>>
+      true -> 
+        letter
+      end
   end
 
   @spec decode(String.t()) :: String.t()
   def decode(cipher) do
+    cipher
+      |> String.replace(~r/[^\w]/, "")
+      |> String.graphemes
+      |> Enum.map(&reverse_codes/1)
+      |> Enum.reduce("", fn (l, acc) -> acc <> l end)
   end
 end
