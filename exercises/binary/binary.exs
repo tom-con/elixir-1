ed10b7245ba9e9a5221fd60dceab0cc3697e3fc4
Thomas Conchie <tconchie@gmail.com>
2018-03-26 17:10:32 -0600

    Completes binary

--- exercises/binary/binary.exs
+++ exercises/binary/binary.exs
@@ -6,5 +6,16 @@ defmodule Binary do
   """
   @spec to_decimal(String.t()) :: non_neg_integer
   def to_decimal(string) do
+    cond do
+      String.match?(string, ~r/[^10]/) ->
+        0
+      true ->
+        String.to_integer(string)
+          |> Integer.digits
+          |> Enum.reverse
+          |> Enum.with_index
+          |> Enum.map(fn({el,i}) -> round(el * :math.pow(2, i)) end)
+          |> Enum.reduce(0, fn(x, acc) -> acc+x end)
+    end 
   end
 end
