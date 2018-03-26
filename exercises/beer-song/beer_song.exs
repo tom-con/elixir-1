7e1464ca557d09bde865c7c91f197a52b6b251b8
Thomas Conchie <tconchie@gmail.com>
2018-03-26 16:21:32 -0600

    Completes beersong

--- exercises/beer-song/beer_song.exs
+++ exercises/beer-song/beer_song.exs
@@ -4,7 +4,29 @@ defmodule BeerSong do
   """
   @spec verse(integer) :: String.t()
   def verse(number) do
-    # Your implementation here...
+    
+    cond do
+      is_integer(number) and number > 2 ->
+        """
+        #{number} bottles of beer on the wall, #{number} bottles of beer.
+        Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+        """
+      is_integer(number) and number === 2 ->
+        """
+        2 bottles of beer on the wall, 2 bottles of beer.
+        Take one down and pass it around, 1 bottle of beer on the wall.
+        """
+      is_integer(number) and number === 1 ->
+        """
+        1 bottle of beer on the wall, 1 bottle of beer.
+        Take it down and pass it around, no more bottles of beer on the wall.
+        """
+      is_integer(number) and number === 0 ->
+        """
+        No more bottles of beer on the wall, no more bottles of beer.
+        Go to the store and buy some more, 99 bottles of beer on the wall.
+        """
+    end
   end
 
   @doc """
@@ -12,6 +34,14 @@ defmodule BeerSong do
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
-    # Your implementation here...
+    l = Enum.reduce(range, "", fn(x, acc) -> acc <> verse(x) <> "\n" end)
+      |> String.trim
+    l <> "\n"
+  end
+
+  def lyrics do
+    l = Enum.reduce(99..0, "", fn(x, acc) -> acc <> verse(x) <> "\n" end)
+      |> String.trim
+    l <> "\n"
   end
 end
